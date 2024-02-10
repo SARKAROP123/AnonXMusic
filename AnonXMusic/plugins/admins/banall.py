f259ccfe08bbef3bc118b7e96512a92f825c7c44
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-02-10 23:53:33 +0530

    Update banall.py
    Signed-off-by: SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>

--- AnonXMusic/plugins/admins/banall.py
+++ AnonXMusic/plugins/admins/banall.py
@@ -2,7 +2,7 @@ from AnonXMusic import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from AnonXMusic.utils.vip_ban import admin_filter
+from AnonXMusic.utils. import admin_filter
 from AnonXMusic.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line
